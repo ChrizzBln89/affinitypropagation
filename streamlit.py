c93abb36c9ed07da80bc8623caab317db43a326a
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-09-17 09:11:59 +0200

    trial

---
+++ streamlit.py
@@ -0,0 +1,142 @@
+import sys
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+import streamlit as st
+from sklearn import covariance
+from sklearn import cluster
+
+st.title("Uber pickups in NYC")
+
+symbol_dict = {
+    "TOT": "Total",
+    "XOM": "Exxon",
+    "CVX": "Chevron",
+    "COP": "ConocoPhillips",
+    "VLO": "Valero Energy",
+    "MSFT": "Microsoft",
+    "IBM": "IBM",
+    "TWX": "Time Warner",
+    "CMCSA": "Comcast",
+    "CVC": "Cablevision",
+    "YHOO": "Yahoo",
+    "DELL": "Dell",
+    "HPQ": "HP",
+    "AMZN": "Amazon",
+    "TM": "Toyota",
+    "CAJ": "Canon",
+    "SNE": "Sony",
+    "F": "Ford",
+    "HMC": "Honda",
+    "NAV": "Navistar",
+    "NOC": "Northrop Grumman",
+    "BA": "Boeing",
+    "KO": "Coca Cola",
+    "MMM": "3M",
+    "MCD": "McDonald's",
+    "PEP": "Pepsi",
+    "K": "Kellogg",
+    "UN": "Unilever",
+    "MAR": "Marriott",
+    "PG": "Procter Gamble",
+    "CL": "Colgate-Palmolive",
+    "GE": "General Electrics",
+    "WFC": "Wells Fargo",
+    "JPM": "JPMorgan Chase",
+    "AIG": "AIG",
+    "AXP": "American express",
+    "BAC": "Bank of America",
+    "GS": "Goldman Sachs",
+    "AAPL": "Apple",
+    "SAP": "SAP",
+    "CSCO": "Cisco",
+    "TXN": "Texas Instruments",
+    "XRX": "Xerox",
+    "WMT": "Wal-Mart",
+    "HD": "Home Depot",
+    "GSK": "GlaxoSmithKline",
+    "PFE": "Pfizer",
+    "SNY": "Sanofi-Aventis",
+    "NVS": "Novartis",
+    "KMB": "Kimberly-Clark",
+    "R": "Ryder",
+    "GD": "General Dynamics",
+    "RTN": "Raytheon",
+    "CVS": "CVS",
+    "CAT": "Caterpillar",
+    "DD": "DuPont de Nemours",
+}
+
+
+symbols, names = np.array(sorted(symbol_dict.items())).T
+
+quotes = []
+
+for symbol in symbols:
+    print("Fetching quote history for %r" % symbol, file=sys.stderr)
+    url = (
+        "https://raw.githubusercontent.com/scikit-learn/examples-data/"
+        "master/financial-data/{}.csv"
+    )
+    quotes.append(pd.read_csv(url.format(symbol)))
+
+close_prices = np.vstack([q["close"] for q in quotes])
+open_prices = np.vstack([q["open"] for q in quotes])
+
+# The daily variations of the quotes are what carry the most information
+variation = close_prices - open_prices
+
+alphas = np.logspace(-1.5, 1, num=10)
+edge_model = covariance.GraphicalLassoCV(alphas=alphas)
+
+# standardize the time series: using correlations rather than covariance
+# former is more efficient for structure recovery
+X = variation.copy().T
+X /= X.std(axis=0)
+edge_model.fit(X)
+
+_, labels = cluster.affinity_propagation(edge_model.covariance_, random_state=0)
+n_labels = labels.max()
+
+for i in range(n_labels + 1):
+    print(f"Cluster {i + 1}: {', '.join(names[labels == i])}")
+
+# Finding a low-dimension embedding for visualization: find the best position of
+# the nodes (the stocks) on a 2D plane
+
+from sklearn import manifold
+
+node_position_model = manifold.LocallyLinearEmbedding(
+    n_components=3, eigen_solver="dense", n_neighbors=6
+)
+
+embedding = node_position_model.fit_transform(X.T).T
+
+
+def create_3d_scatterplot(embedding):
+    X = embedding[0]
+    Y = embedding[1]
+    Z = embedding[2]
+
+    # Create the 3D scatterplot
+    fig = go.Figure(data=[go.Scatter3d(x=X, y=Y, z=Z, mode="markers")])
+
+    # Set plot layout (optional)
+    fig.update_layout(
+        scene=dict(
+            xaxis_title="X Axis Title",
+            yaxis_title="Y Axis Title",
+            zaxis_title="Z Axis Title",
+        )
+    )
+
+    # Show the plot (you can also save it to a file)
+    fig.show()
+
+
+# Assuming you have the 'embedding' data as a 3D array or list of lists
+embedding = embedding  # Replace this with your actual data
+fig = create_3d_scatterplot(embedding)
+
+st.plotly_chart(fig, use_container_width=True)
