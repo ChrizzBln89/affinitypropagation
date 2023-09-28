f381dcdef9dd8238ecc82ba22932fc2b27513cc9
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-09-28 12:43:13 +0200

    Auto upload

--- modules/class_peer_group.py
+++ modules/class_peer_group.py
@@ -4,6 +4,7 @@ import pandas as pd
 class peer_group:
     def __init__(self):
         self.data = pd.read_csv("Data/info_merged.csv", header=0, index_col=0)
+        self.peer_info_data = pd.DataFrame()
         self.peer_companies = []
 
     def add_company(self, ticker: str):
@@ -12,5 +13,8 @@ class peer_group:
         return self.peer_companies
 
     def company_data(self) -> pd.DataFrame():
-        self.data = self.data[self.data["symbol"].isin(self.peer_companies)]
-        return self.data
+        self.peer_info_data = self.data[self.data["symbol"].isin(self.peer_companies)]
+        return self.peer_info_data
+
+    def beta_calc(self) -> pd.DataFrame():
+        pass
