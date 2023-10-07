4dacca394e473a22b20e759baec79e19f1819d50
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-10-07 15:04:46 +0200

    Auto upload

--- quotes_bigquery.py
+++ quotes_bigquery.py
@@ -1,26 +1,31 @@
 import pandas as pd
 from google.oauth2 import service_account
 from pandas_gbq import to_gbq
+from dagster import asset
 
-project_id = "flash-realm-401106"
-dataset_id = "valuationhub"
-table_id = "quotes"
-credentials_file = "flash-realm-401106-a0bf29a37df7.json"
 
-df = pd.read_csv("data/history_merged.csv")
+@asset
+def upload():
+    project_id = "flash-realm-401106"
+    dataset_id = "valuationhub"
+    table_id = "quotes"
+    credentials_file = "flash-realm-401106-a0bf29a37df7.json"
 
-credentials = service_account.Credentials.from_service_account_file(
-    credentials_file, scopes=["https://www.googleapis.com/auth/bigquery"]
-)
+    df = pd.read_csv("data/history_merged.csv")
 
-destination_table = f"{project_id}.{dataset_id}.{table_id}"
+    credentials = service_account.Credentials.from_service_account_file(
+        credentials_file, scopes=["https://www.googleapis.com/auth/bigquery"]
+    )
 
-to_gbq(
-    df,
-    destination_table,
-    project_id=project_id,
-    if_exists="replace",  # Choose whether to replace or append to the table
-    credentials=credentials,  # Specify the schema for the table
-)
+    destination_table = f"{project_id}.{dataset_id}.{table_id}"
 
-print(f"DataFrame uploaded to {destination_table} in BigQuery.")
+    to_gbq(
+        df,
+        destination_table,
+        project_id=project_id,
+        if_exists="replace",  # Choose whether to replace or append to the table
+        credentials=credentials,  # Specify the schema for the table
+    )
+
+    print(f"DataFrame uploaded to {destination_table} in BigQuery.")
+    return df
