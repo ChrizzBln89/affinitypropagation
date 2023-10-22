d870a20c9f81073e5ef10a3f5ef5147b26d34e89
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-10-22 18:38:53 +0200

    Auto upload

--- frontpage.py
+++ frontpage.py
@@ -2,27 +2,7 @@ import streamlit as st
 import pandas as pd
 from modules.pages.peer_group_user import peer_group_user
 from modules.pages.beta_page import beta_page
-from modules.pages.peergroup_page import peergroup_page
-from modules.pages.db import db
-from sidebar import sidebar
 
 
 st.set_page_config(layout="wide")
-
-st.sidebar.header("Page Navigation", divider="blue")
-
-
-add_selectbox = st.sidebar.selectbox(
-    "", ("Peer Group Selection", "Beta Calculation", "Company Database")
-)
-
-sidebar(peer_group_user=peer_group_user)
-
-if add_selectbox == "Peer Group Selection":
-    peergroup_page(peer_group_user=peer_group_user)
-
-if add_selectbox == "Beta Calculation":
-    beta_page(peer_group_user=peer_group_user)
-
-if add_selectbox == "Company Database":
-    db()
+beta_page(peer_group_user=peer_group_user)
--- modules/pages/beta_page.py
+++ modules/pages/beta_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime
-from modules.pages import peer_group_user
+from peer_group_user import peer_group_user
+from peer_group_page import peergroup_page
 import plotly.graph_objs as go
 
 
@@ -9,67 +10,39 @@ def beta_page(peer_group_user=peer_group_user) -> None:
     tabs_peer_group = st.tabs(
         ["Peer Group Finder", "Peer Group Overview", "Beta Calculation", "Multiples"]
     )
-    tabs_peer_group = st.tabs(peer_group_user.peer_companies)
 
-    beta_dict = peer_group_user.beta_calc()
+    with tabs_peer_group[0]:
+        peergroup_page(peer_group_user=peer_group_user)
 
-    for i, company in enumerate(peer_group_user.peer_companies):
-        with tabs_peer_group[i]:
-            st.subheader(f"{company} Beta")
-            st.line_chart(beta_dict[company]["beta"])
-            # Create a scatter plot
-            fig = go.Figure()
-            fig.add_trace(
-                go.Scatter(
-                    x=beta_dict[company]["return_index"],
-                    y=beta_dict[company]["return_stock"],
-                    mode="markers",
-                    name="Returns",
+    with tabs_peer_group[2]:
+        tabs_peer_group = st.tabs(peer_group_user.peer_companies)
+        beta_dict = peer_group_user.beta_calc()
+
+        for i, company in enumerate(peer_group_user.peer_companies):
+            with tabs_peer_group[i]:
+                st.subheader(f"{company} Beta")
+                st.line_chart(beta_dict[company]["beta"])
+                # Create a scatter plot
+                fig = go.Figure()
+                fig.add_trace(
+                    go.Scatter(
+                        x=beta_dict[company]["return_index"],
+                        y=beta_dict[company]["return_stock"],
+                        mode="markers",
+                        name="Returns",
+                    )
                 )
-            )
-            fig.update_layout(
-                title="Stock vs Index Returns",
-                xaxis_title="Index Returns",
-                yaxis_title="Stock Returns",
-            )
-
-            # Display the scatter plot in Streamlit
-            st.plotly_chart(fig)
-
-            st.table(beta_dict[company].reset_index(drop=True))
-            # Create a scatter plot
-            fig = go.Figure()
-            fig.add_trace(
-                go.Scatter(
-                    x=beta_dict[company]["return_index"],
-                    y=beta_dict[company]["return_stock"],
-                    mode="markers",
-                    marker=dict(
-                        color=[
-                            "blue" if r == "return_index" else "red"
-                            for r in beta_dict[company].reset_index().columns
-                        ],
-                        size=10,
-                        symbol=[
-                            "circle" if r == "return_index" else "square"
-                            for r in beta_dict[company].reset_index().columns
-                        ],
-                        line=dict(
-                            color=[
-                                "blue" if r == "return_index" else "red"
-                                for r in beta_dict[company].reset_index().columns
-                            ],
-                            width=2,
-                        ),
-                    ),
-                    name="Returns",
+                fig.update_layout(
+                    title="Stock vs Index Returns",
+                    xaxis_title="Index Returns",
+                    yaxis_title="Stock Returns",
                 )
-            )
-            fig.update_layout(
-                title="Stock vs Index Returns",
-                xaxis_title="Index Returns",
-                yaxis_title="Stock Returns",
-            )
 
-            # Display the scatter plot in Streamlit
-            st.plotly_chart(fig)
+                # Display the scatter plot in Streamlit
+                st.plotly_chart(fig)
+
+                st.table(beta_dict[company].reset_index(drop=True))
+
+
+if __name__ == "__main__":
+    beta_page()
